8bc945346317296b6a69ddaf93cc316a0aed59b7
Hendri Pretorius <pretorh@gmail.com>
2016-03-21 10:31:14 +0200

    add empty lines at the end of each section

--- src/generator.c
+++ src/generator.c
@@ -1,17 +1,22 @@
 #include "generator.h"
 #include <stdio.h>
 
+#define EMPTY_LINE puts("");
+
 void extract(const char *archive, const char *name) {
     printf("tar xf %s\n", archive);
     printf("cd %s\n", name);
+    EMPTY_LINE
 }
 
 void make_build_dir() {
     printf("mkdir build\n");
     printf("cd build\n");
+    EMPTY_LINE
 }
 
 void cleanup(const char *dir_name, int nested_dir) {
     printf("cd ../%s\n", nested_dir ? "../" : "");
     printf("rm -rf %s\n", dir_name);
+    EMPTY_LINE
 }
