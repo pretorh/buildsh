5c86e9451e5f8b3a1ac4661dad40b49199e6cae8
Hendri Pretorius <pretorh@gmail.com>
2016-08-07 08:57:35 +0200

    extract functions
    extract functions out of generator_finalize_setup
    configure, build, test, install, cleanup

--- src/generator.c
+++ src/generator.c
@@ -29,6 +29,39 @@ void generator_extract_source(struct Settings *settings) {
         settings->archive, settings->name, current);
 }
 
+void generator_configure(struct Settings *settings) {
+    APPLY_DEFAULT(settings->configure_commands,
+        "%s%s/configure%s\n",
+        settings->config_env, settings->config_dir, settings->config_options);
+}
+
+void generator_build(struct Settings *settings) {
+    APPLY_DEFAULT(settings->build_commands,
+        "make --jobs %d %s\n",
+        settings->max_make_jobs, settings->make_options);
+}
+
+void generator_test(struct Settings *settings) {
+    APPLY_DEFAULT(settings->test_commands,
+        "make check --jobs %d\n",
+        settings->max_make_jobs);
+}
+
+void generator_install(struct Settings *settings) {
+    APPLY_DEFAULT(settings->install_commands,
+        "%smake%s install\n",
+        settings->install_sudo ? "sudo " : "",
+        settings->install_options);
+}
+
+void generator_cleanup(struct Settings *settings) {
+    APPLY_DEFAULT(settings->cleanup_commands,
+        "cd %s\n"
+        "rm -rf %s\n",
+        settings->build_outside_sources ? "../.." : "..",
+        settings->name);
+}
+
 // public generator functions
 
 void generator_init(struct Settings *settings) {
@@ -50,30 +83,13 @@ void generator_finalize_setup(struct Settings *settings) {
 
     if (settings->build_outside_sources)
         generator_create_build_dir(settings->source_setup);
-
     if (settings->do_configure)
-        APPLY_DEFAULT(settings->configure_commands,
-            "%s%s/configure%s\n",
-            settings->config_env, settings->config_dir, settings->config_options);
-
+        generator_configure(settings);
     if (settings->do_build)
-        APPLY_DEFAULT(settings->build_commands,
-            "make --jobs %d %s\n",
-            settings->max_make_jobs, settings->make_options);
-
+        generator_build(settings);
     if (settings->do_test)
-        APPLY_DEFAULT(settings->test_commands,
-            "make check --jobs %d\n",
-            settings->max_make_jobs);
+        generator_test(settings);
 
-    APPLY_DEFAULT(settings->install_commands,
-        "%smake%s install\n",
-        settings->install_sudo ? "sudo " : "",
-        settings->install_options);
-
-    APPLY_DEFAULT(settings->cleanup_commands,
-        "cd %s\n"
-        "rm -rf %s\n",
-        settings->build_outside_sources ? "../.." : "..",
-        settings->name);
+    generator_install(settings);
+    generator_cleanup(settings);
 }
